33112a919ffe0f6ada15890292153f37fce49ad5
capslock35 <kaandura@gmail.com>
2019-04-17 12:59:37 +0300

    Create main.js

---
+++ backend/main.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+// import api route
+var api = require('./api/Api');
+
+// create servers
+var httpServer = http.createServer(app);
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// for parsing cookies
+app.use(cookieParser());
+
+app.use(function(req,res,next){
+	res.sendResponse = res.send;
+	res.send = function(response){
+		res.sendResponse(response);
+	};
+	next();
+});
+
+// api route
+app.use('/api/',api);
+
+//opening page
+app.use('/', function (req, res) {
+	res.send("opening page!");
+});
+
+// start servers
+httpServer.listen(1010,function(){
+    console.log('HTTP SERVER listening on port 1010!');
+});
