9405783b9b54145b5bd5df73c8762bc03f41f8e6
capslock35 <kaandura@gmail.com>
2019-04-17 13:00:13 +0300

    Create Api.js

---
+++ backend/api/Api.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var db = require('./DataBase');
+var user=require('./User');
+
+db.connect(function(err){
+  if(err === null){
+    console.log("DB CONNECTED!");
+  }
+  else{
+    console.log("DB CONNECTION FAILED!");
+    console.log(err);
+  }
+});
+
+router.use('/register',function(req,res){
+  if(!db.isConnected()){
+    res.send("9");
+    return;
+  }
+  user.register(db,req,res);
+});
+module.exports = router;
