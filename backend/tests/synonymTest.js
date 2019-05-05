c7d033dd72b3c0ca0e1907b2812f23c1d1bd6865
kaandura <kaandura@gmail.com>
2019-05-05 23:03:30 +0300

    Fix synonym test

--- backend/tests/synonymTest.js
+++ backend/tests/synonymTest.js
@@ -1,65 +1,55 @@
-
-
 var request = require('request');
 
 async function synonymTest() {
-
     return new Promise((resolve, reject) => {
-
       request("http://localhost:8080/api/synonym?word=bad", function (error, response, body) {
           resolve(testSynonym(body));
       });
-
     });
-
 }
 
 
 function testSynonym(body) {
 
-    let body_obj = JSON.parse(body);
+  let body_obj = JSON.parse(body);
 
-    let bad_json = {
-        "word": "bad",
-        "synonyms": [
-            "badly",
-            "badness",
-            "big",
-            "uncollectible",
-            "forged",
-            "unfit",
-            "unsound",
-            "defective",
-            "regretful",
-            "sorry",
-            "high-risk",
-            "risky",
-            "speculative",
-            "tough",
-            "spoiled",
-            "spoilt"
-        ]
-    };
+  let bad_json = {
+      "word": "bad",
+      "synonyms": [
+          "badly",
+          "badness",
+          "big",
+          "uncollectible",
+          "forged",
+          "unfit",
+          "unsound",
+          "defective",
+          "regretful",
+          "sorry",
+          "high-risk",
+          "risky",
+          "speculative",
+          "tough",
+          "spoiled",
+          "spoilt"
+      ]
+  };
 
-    return JSON.stringify(body_obj) === JSON.stringify(bad_json);
+  return JSON.stringify(body_obj) === JSON.stringify(bad_json);
 
 }
 
 
 async function asyncSynonymTest(){
 
-    let res = await synonymTest();
+  let res = await synonymTest();
 
-    console.log(res);
-
-    if(res){
-        return "working";
-    }else{
-        return "not working";
-    }
+  if(res){
+      return "working";
+  }else{
+      return "not working";
+  }
 
 }
-
-
 
 module.exports = asyncSynonymTest;
