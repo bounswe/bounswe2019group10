df09dc7f0b9b70890eb04d484d8e01c9dc0142b1
Fahri Can Şanlı <fahrican@armut.com>
2019-10-03 11:24:38 +0300

    mainActivity changed

--- YallpAndroid/app/src/main/java/com/example/yallp_android/MainActivity.java
+++ YallpAndroid/app/src/main/java/com/example/yallp_android/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.yallp_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
