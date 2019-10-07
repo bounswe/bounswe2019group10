4ac2c69e1c8dce876c9c0c1a5d6ed442282869c9
yavuz.demir <yavuz.demir@trendyol.com>
2019-10-07 21:42:11 +0300

    readme updated by hatice

--- backend/src/main/java/com/example/backend/service/MemberService.java
+++ backend/src/main/java/com/example/backend/service/MemberService.java
@@ -6,7 +6,6 @@ import com.example.backend.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class MemberService {
