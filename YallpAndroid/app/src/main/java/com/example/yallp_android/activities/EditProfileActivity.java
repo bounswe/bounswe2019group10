8505642e15da748b2020b3082624b091747bd854
MelihDemir99 <melihdemir299@gmail.com>
2019-11-16 18:38:52 +0300

    Edit profile page created

---
+++ YallpAndroid/app/src/main/java/com/example/yallp_android/activities/EditProfileActivity.java
@@ -0,0 +1,24 @@
+package com.example.yallp_android.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yallp_android.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_edit_profile);
+
+    }
+}
