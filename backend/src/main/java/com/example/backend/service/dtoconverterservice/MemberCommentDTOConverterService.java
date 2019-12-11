93782e1756bf839d02741b11f7f4f306ddf30c1e
yavuz.demir <yavuz.demir@trendyol.com>
2019-12-11 14:08:49 +0300

    Added MemberComment to MemberCommentDTO converter service

---
+++ backend/src/main/java/com/example/backend/service/dtoconverterservice/MemberCommentDTOConverterService.java
@@ -0,0 +1,42 @@
+package com.example.backend.service.dtoconverterservice;
+
+import com.example.backend.model.member.MemberComment;
+import com.example.backend.model.member.MemberCommentDTO;
+
+import com.example.backend.service.member.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public
+class MemberCommentDTOConverterService {
+
+    @Autowired
+    JwtUserDetailsService jwtUserDetailsService;
+
+    public MemberCommentDTO apply(MemberComment memberComment) {
+        MemberCommentDTO memberCommentDTO = new MemberCommentDTO();
+        memberCommentDTO.setId(memberComment.getId());
+        memberCommentDTO.setMemberId(memberComment.getMemberId());
+        memberCommentDTO.setMemberName(jwtUserDetailsService.getUsernameById(memberComment.getMemberId()));
+        memberCommentDTO.setCommentatorId(memberComment.getCommentatorId());
+        memberCommentDTO.setCommentatorName(jwtUserDetailsService.getUsernameById(memberComment.getCommentatorId()));
+        memberCommentDTO.setCreatedAt(memberComment.getCreatedAt());
+        memberCommentDTO.setUpdatedAt(memberComment.getUpdatedAt());
+        memberCommentDTO.setComment(memberComment.getComment());
+        return memberCommentDTO;
+    }
+
+    public List<MemberCommentDTO> applyAll(List<MemberComment> memberComments){
+        List<MemberCommentDTO> list = new ArrayList<>();
+
+        memberComments.forEach(memberComment -> {
+            list.add(apply(memberComment));
+        });
+
+        return list;
+    }
+}
