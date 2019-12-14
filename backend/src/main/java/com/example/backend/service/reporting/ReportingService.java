b4cea6bd36bd69a2d8291ec4ccec8b148a0eed08
selim-karaduman <selim.karaduman07@gmail.com>
2019-12-15 01:58:46 +0300

    Implementation of report service

---
+++ backend/src/main/java/com/example/backend/service/reporting/ReportingService.java
@@ -0,0 +1,40 @@
+package com.example.backend.service.reporting;
+
+
+import com.example.backend.model.reporting.Report;
+import com.example.backend.model.reporting.ReportCause;
+import com.example.backend.model.reporting.ReportDTO;
+import com.example.backend.model.reporting.ReportRequest;
+import com.example.backend.repository.reporting.ReportCauseRepository;
+import com.example.backend.repository.reporting.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReportingService {
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Autowired
+    private ReportCauseRepository reportCauseRepository;
+
+    public ReportRequest addNewReport(ReportRequest reportRequest, String username){
+        Report report = new Report();
+        report.setCause(reportRequest.getCause());
+        report.setExplanation(reportRequest.getOptionalExplanation());
+        report.setReportingUsername(username);
+        report.setReportedUsername(reportRequest.getReported_username());
+        reportRepository.save(report);
+        return reportRequest;
+    }
+
+    public List<ReportCause> getReportStructure(){
+        ReportDTO reportDTO = new ReportDTO();
+        return reportCauseRepository.findAll();
+    }
+
+
+}
