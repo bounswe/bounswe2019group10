b107760ea5b4cf6c44ad3c45daab2980e57506ad
Hatice <melikeecevit@hotmail.com>
2019-12-16 09:55:39 +0300

    added the new upload image endpoint

---
+++ backend/src/main/java/com/example/backend/controller/image/ImageController.java
@@ -0,0 +1,24 @@
+package com.example.backend.controller.image;
+
+import com.example.backend.service.aws.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/image/")
+public class ImageController {
+
+    @Autowired
+    private AmazonClient amazonClient;
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+}
