9d9fa4d0d4a73f69bbcdbe4e609d89d074821658
yavuz.demir <yavuz.demir@trendyol.com>
2019-12-18 18:34:00 +0300

    CREATED endpoints for image create and see annotations on image

--- backend/src/main/java/com/example/backend/controller/annotation/AnnotationController.java
+++ backend/src/main/java/com/example/backend/controller/annotation/AnnotationController.java
@@ -2,6 +2,8 @@ package com.example.backend.controller.annotation;
 
 import com.example.backend.model.annotation.Annotation;
 import com.example.backend.model.annotation.AnnotationDTO;
+import com.example.backend.model.annotation.ImageAnnotation;
+import com.example.backend.model.annotation.ImageAnnotationDTO;
 import com.example.backend.service.annotation.AnnotationService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,46 +21,62 @@ public class AnnotationController {
     @Autowired
     AnnotationService annotationService;
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     @ApiOperation(value = "Get annotation")
-    public ResponseEntity<Map<String, Object>> getAnnotation(@PathVariable int id){
+    public ResponseEntity<Map<String, Object>> getAnnotation(@PathVariable int id) {
         return ResponseEntity.ok(annotationService.getAnnotation(id));
     }
 
     @GetMapping("/all")
     @ApiOperation(value = "Get all annotations")
-    public ResponseEntity<List<Map<String, Object>>> getAllAnnotations(){
+    public ResponseEntity<List<Map<String, Object>>> getAllAnnotations() {
         return ResponseEntity.ok(annotationService.getAllAnnotations());
     }
 
-    @GetMapping("all/{writingId}")
-    @ApiOperation(value = "Get annotation")
-    public ResponseEntity<List<Map<String, Object>>> getAllAnnotationsByWriting(@PathVariable int writingId){
+    @GetMapping("/all/{writingId}")
+    @ApiOperation(value = "Get all annotations of a writing text")
+    public ResponseEntity<List<Map<String, Object>>> getAllAnnotationsByWriting(@PathVariable int writingId) {
         return ResponseEntity.ok(annotationService.getAllAnnotationsByWriting(writingId));
     }
-/*
-    @GetMapping("/writing/{writingResultId}")
-    @ApiOperation(value = "Get annotations of a writing")
-    public ResponseEntity<List<Annotation>> getAnnotations(@PathVariable int writingResultId){
-        return ResponseEntity.ok(annotationService.findAllByWriting(writingResultId));
-    }
-*/
+
     @PostMapping("/create")
     @ApiOperation(value = "Create annotation for a writing")
-    public ResponseEntity<Annotation> createAnnotation(@RequestBody AnnotationDTO annotationDTO){
+    public ResponseEntity<Annotation> createAnnotation(@RequestBody AnnotationDTO annotationDTO) {
         return ResponseEntity.ok(annotationService.createAnnotation(annotationDTO));
     }
 
     @PostMapping("/delete")
     @ApiOperation(value = "delete annotation")
-    public ResponseEntity<String> deleteAnnotation(@RequestParam(value = "id") int id){
+    public ResponseEntity<String> deleteAnnotation(@RequestParam(value = "id") int id) {
         return ResponseEntity.ok(annotationService.deleteAnnotation(id));
     }
 
     @PostMapping("/update")
     @ApiOperation(value = "Update annotation that you wrote")
-    public ResponseEntity<Annotation> updateAnnotation(@RequestBody AnnotationDTO annotationDTO){
+    public ResponseEntity<Annotation> updateAnnotation(@RequestBody AnnotationDTO annotationDTO) {
         return ResponseEntity.ok(annotationService.updateAnnotation(annotationDTO));
+    }
+
+
+    ///////////////////////////// IMAGE ANNOTATION ////////////////////////
+
+    @GetMapping("/image/{id}")
+    @ApiOperation(value = "Get image annotation")
+    public ResponseEntity<Map<String, Object>> getImageAnnotation(@PathVariable int id) {
+        return ResponseEntity.ok(annotationService.getAnnotation(id));
+    }
+
+    @GetMapping("/all")
+    @ApiOperation(value = "Get Image annotations for an image")
+    public ResponseEntity<List<Map<String, Object>>> getAllImageAnnotationsByImageUrl(@RequestParam(value = "url") String imageUrl) {
+        return ResponseEntity.ok(annotationService.getAllImageAnnotationsByImageUrl(imageUrl));
+    }
+
+
+    @PostMapping("/image/create")
+    @ApiOperation(value = "Create image annotation for a writing result")
+    public ResponseEntity<ImageAnnotation> createImageAnnotation(@RequestBody ImageAnnotationDTO imageAnnotationDTO) {
+        return ResponseEntity.ok(annotationService.createImageAnnotation(imageAnnotationDTO));
     }
 
 }
