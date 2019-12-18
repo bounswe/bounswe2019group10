aff888252588eac11ccbb7ce5c580194f114a010
yavuz.demir <yavuz.demir@trendyol.com>
2019-12-18 18:32:00 +0300

    CREATED image annotation dto

---
+++ backend/src/main/java/com/example/backend/model/annotation/ImageAnnotationDTO.java
@@ -0,0 +1,67 @@
+package com.example.backend.model.annotation;
+
+import java.sql.Timestamp;
+
+public class ImageAnnotationDTO {
+
+
+    private String imageUrl;
+
+    private String annotationText;
+
+    private Integer x;
+
+    private Integer y;
+
+    private Integer w;
+
+    private Integer h;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAnnotationText() {
+        return annotationText;
+    }
+
+    public void setAnnotationText(String annotationText) {
+        this.annotationText = annotationText;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public Integer getW() {
+        return w;
+    }
+
+    public void setW(Integer w) {
+        this.w = w;
+    }
+
+    public Integer getH() {
+        return h;
+    }
+
+    public void setH(Integer h) {
+        this.h = h;
+    }
+}
