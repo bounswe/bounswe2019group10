7a0ff519685ba661aa6f9eee741d28aab5e6397b
Hatice <melikeecevit@hotmail.com>
2019-12-19 10:11:25 +0300

    changed the variables and added variables to aws machine

--- backend/src/main/java/com/example/backend/controller/image/ImageController.java
+++ backend/src/main/java/com/example/backend/controller/image/ImageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/image/")
+@RequestMapping("/image")
 public class ImageController {
 
     @Autowired
